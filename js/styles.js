7617bf54a066fdbab934065b0bc77590f9c0ed7c
Dekoworld <karituderrick@gmail.com>
2019-09-19 16:06:02 +0300

    added functions and linked with index.html

---
+++ js/styles.js
@@ -0,0 +1,7 @@
+var add = function(a,b,c) {
+return a + b + c;
+};
+var a =parseInt(prompt("enter number"));
+var b =parseInt(prompt("enter sec number"));
+var c =parseInt(prompt("enter third number"))
+alert(add(a, b, c));
